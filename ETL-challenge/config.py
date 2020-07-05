d951aa8e047ac6571d3b5efc259c3be8877ece7c
CommandLine <98483@acs.org>
2020-07-05 18:04:33 -0400

    Load ETL Project

---
+++ ETL-challenge/config.py
@@ -0,0 +1,11 @@
+#SalesForce api
+sf_username = 'oyasir@gmail.com'
+sf_password = 'Summer16!'
+sf_security_token = 'zM6M31Rsvl2pzpfUuUbAGrJ1r'
+
+#MySql 
+remote_db_endpoint = 'database-1.czlbzbpdgtgg.us-east-1.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_db_user = 'admin'
+remote_db_pwd = 'Summer16!'
+remote_db_name = 'gwsis'
