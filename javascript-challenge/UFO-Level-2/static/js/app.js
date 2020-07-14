a3dbbe097d36390ff178fdb2ef5fc7a04c859691
CommandLine <98483@acs.org>
2020-07-13 22:11:22 -0400

    user loop to filter & detects changes to html filter fields

--- javascript-challenge/UFO-Level-2/static/js/app.js
+++ javascript-challenge/UFO-Level-2/static/js/app.js
@@ -1,62 +1,54 @@
-
-var tbody = d3.select('#main-table')
+var tbody = d3.select('#main-table');
 
 var filterbutton = d3.select('#filter-btn');
 filterbutton.on('click', runtablefilter);
 
-var clearbutton = d3.select('#clear-filter-btn')
-clearbutton.on('click', runclearfilter)
+var clearbutton = d3.select('#clear-filter-btn');
+clearbutton.on('click', runclearfilter);
 
-function runclearfilter(){
-    tbody.html("");
-    data.forEach(buildtable)
-    document.getElementById("my-form").reset();
-    filtereddata = []
-   }
+//build array of filter words pulled from HTML code. 
+htmlfilterIDs = [];
+var formControl = d3.selectAll('.form-control');
+var filterLength = formControl._groups[0].length;
+for(var i=0; i < filterLength; i++ ){
+    filterid = formControl._groups[0][i]['id']
+    htmlfilterIDs.push(filterid)
+};
 
+//Generate a table:
+//table is unfilterd if the function is passed the data array
+//table is filtered if function is called from runtablefilter() and passed the filtereddata array
 function buildtable(tableinfo){
     var row = tbody.append('tr')
     var sightingvalues = Object.values(tableinfo)
     sightingvalues.forEach(sightingvalue => {
        var cell = row.append('td')
        cell.text(sightingvalue)
-   }); 
-   
+   });
 };
 
+//Generate a filtered the filtereddata array based on filter values from the webpage
 function runtablefilter(){
     tbody.html("");
     var filtereddata = data
 
-    var dateElement = d3.select('#datetime')
-    var dateValue = dateElement.property('value')  
-    if (dateValue !="") {
-        filtereddata = filtereddata.filter(sighting => sighting.datetime == dateValue)
-    }
-
-    var cityElement = d3.select('#city')
-    var cityValue = cityElement.property('value')
-    if (cityValue !=""){
-        filtereddata = filtereddata.filter(sighting => sighting.city == cityValue)
-    }
-
-    var stateElement = d3.select('#state')
-    var stateValue = stateElement.property('value')
-    if (stateValue != ""){
-        filtereddata = filtereddata.filter(sighting => sighting.state == stateValue)
-    }
-
-    var shapeElement = d3.select('#shape')
-    var shapeValue = shapeElement.property('value')
-    if (shapeValue != ""){
-        filtereddata = filtereddata.filter(sighting => sighting.shape == shapeValue)
-    }
-    
+    htmlfilterIDs.forEach(iditem => {
+        var idElement = d3.select(`#${iditem}`)
+        var idValue = idElement.property('value') 
+        if (idValue !="") {
+            filtereddata = filtereddata.filter(sighting => sighting[`${iditem}`] == idValue) 
+        } 
+    });      
+   
     filtereddata.forEach(buildtable)
-   console.log(`after filter func: ${filtereddata.length}`)//debug
+}; 
+
+//Resets the table by clearing the filters and regenerating the complete table using the data array
+function runclearfilter(){
+    tbody.html("");
+    data.forEach(buildtable)
+    document.getElementById("my-form").reset();
+    filtereddata = []
 };
 
-
-data.forEach(sightingobjects => {
-  buildtable(sightingobjects)
-});
+data.forEach(buildtable)
